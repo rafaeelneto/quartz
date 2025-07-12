d2c423e229c519aa696764211cb8f81c876d754b
Rafael Gomes <5102522+rafaeelneto@users.noreply.github.com>
2025-07-12 16:29:20 -0300

    change url name

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "rafaeel.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
